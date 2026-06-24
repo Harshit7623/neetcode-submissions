c7bfebe9948537242734c8af1436f7859a5c9937
Harshit Tiwari <harshit31250@gmail.com>
2026-06-24 22:22:32 +0530

    Add: time-based-key-value-store - submission-0

---
+++ Data Structures & Algorithms/time-based-key-value-store/submission-0.cpp
@@ -0,0 +1,39 @@
+class TimeMap {
+private:
+    unordered_map<string, vector<pair<int, string>>> values;
+public:
+    TimeMap() {
+    }
+    
+    void set(string key, string value, int timestamp) {
+        values[key].push_back({timestamp, value});
+    }
+    
+    string get(string key, int timestamp) {
+      
+        if (values.find(key) == values.end()) {
+            return "";
+        }
+        
+        
+        const vector<pair<int, string>>& vec = values[key];
+        
+        int left = 0;
+        int right = vec.size() - 1;
+        string result = "";
+        
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            
+            if (vec[mid].first <= timestamp) {
+                result = vec[mid].second; 
+                left = mid + 1;
+            } 
+            else {
+                right = mid - 1;
+            }
+        }
+        
+        return result;
+    }
+};
